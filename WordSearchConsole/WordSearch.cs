7b304cda08b4c2f98be62c01d1090f2a1e696f8c
jmoses45 <jmoses45@yahoo.com>
2018-10-15 18:12:15 -0500

    Created function to get search words and companion test function.

--- WordSearchConsole/WordSearch.cs
+++ WordSearchConsole/WordSearch.cs
@@ -8,9 +8,17 @@ namespace WordSearchConsole
 {
 	public class WordSearch
 	{
-		public int ReturnInt(int i)
+		public string[] GetSearchWords(string puzzleInput)
 		{
-			return i;
+			//Get a substring of the puzzle input string consisting of the first line of 
+			//character minus the \n character
+			string firstLineSubstring = puzzleInput.Substring(0, puzzleInput.IndexOf('\n') - 1);
+
+			//Split the substring by commas to get an array of search words
+			string[] result = firstLineSubstring.Split(',');
+			
+			//return the array of search words.
+			return result;
 		}
 	}
 }
